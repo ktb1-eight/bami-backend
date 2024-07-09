1ad8802555fc0ec59630b21c10a10a09e6e6c185
alswp006 <alswp006@github.com>
2024-07-09 15:42:31 +0900

    first commit

---
+++ src/main/java/com/example/bami/BamiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bami/BamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
