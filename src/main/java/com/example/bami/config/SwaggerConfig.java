93a521932e0018d274950b90396bed398c61b019
alswp006 <alswp006@github.com>
2024-07-11 15:21:07 +0900

    Swagger 설정 완료

--- src/main/java/com/example/bami/config/SwaggerConfig.java
+++ src/main/java/com/example/bami/config/SwaggerConfig.java
@@ -28,8 +28,7 @@ public class SwaggerConfig {
     }
     private Info apiInfo() {
         return new Info()
-                .title("API Test") // API의 제목
-                .description("Let's practice Swagger UI") // API에 대한 설명
+                .title("bami API Test") // API의 제목
                 .version("1.0.0"); // API의 버전
     }
 }
