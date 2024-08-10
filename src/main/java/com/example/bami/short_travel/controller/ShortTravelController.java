edb314777ad033544fddf2074de1e9d0b63fbab2
alswp006 <alswp006@github.com>
2024-08-10 15:44:25 +0900

    [Feature] 단기 여행 일정 선호도 조사 데이터 수신 및 확인 기능 추가

---
+++ src/main/java/com/example/bami/short_travel/controller/ShortTravelController.java
@@ -0,0 +1,20 @@
+package com.example.bami.short_travel.controller;
+
+import com.example.bami.short_travel.dto.ShortTravelDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class ShortTravelController {
+
+    @PostMapping("/submit")
+    public void submitTravelPlan(@RequestBody ShortTravelDTO shortTravelDTO) {
+        log.info("Received travel plan:");
+        log.info("Companion: {}", shortTravelDTO.getCompanion());
+        log.info("Transport: {}", shortTravelDTO.getTransport());
+        log.info("Preferences: {}", shortTravelDTO.getPreferences());
+        log.info("Location: {}", shortTravelDTO.getLocation());
+    }
+}
