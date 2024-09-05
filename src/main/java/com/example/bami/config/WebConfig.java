f6721b78f465c9b5ab52b7fa5044164b762f18be
LocKey <rudfhr97@naver.com>
2024-09-05 16:50:16 +0900

    [Update] localhost endpoint 환경변수 설정

--- src/main/java/com/example/bami/config/WebConfig.java
+++ src/main/java/com/example/bami/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.bami.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${app.end-point}")
+    private String endPoint;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 API 경로에 대해 CORS 설정
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://127.0.0.1:3000", "http://127.0.0.1:8080") // React 애플리케이션의 도메인
+                .allowedOrigins("http://" + endPoint + ":8080", "http://" + endPoint + ":3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
