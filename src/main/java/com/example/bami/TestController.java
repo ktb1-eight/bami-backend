e0572835c94db0f1851c96affbe65b1212b61c10
alswp006 <alswp006@github.com>
2024-09-05 19:43:34 +0900

    [Update] 테스트 API URL 구현

---
+++ src/main/java/com/example/bami/TestController.java
@@ -0,0 +1,16 @@
+package com.example.bami;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @RequestMapping("api/test")
+    public void test(){
+        log.info("성공");
+        System.out.println("성공했습니다.");
+    }
+}
